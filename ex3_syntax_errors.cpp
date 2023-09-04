ebca19e80e23c5b6a3023102428c29bc372d87a8
L Carver <wifihum@gmail.com>
2023-09-04 11:33:03 -0700

    Add project files.

---
+++ ex3_syntax_errors.cpp
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+// ex3_syntax_errors.cpp
+// 
+// Exercise 3: find the syntax errors in this code
+//------------------------------------------------------------------------------
+
+#include iostream
+
+using namespace std;
+
+//------------------------------------------------------------------------------
+// entry point
+//------------------------------------------------------------------------------
+int main() {
+    */ Can you fix the problems with this code? */
+
+    // declare 3 integers
+    int three, user_input, total
+
+    // initialize variable three to value 3
+    three == 3;
+
+    Cout < "\nThe value of three is " << three;
+
+    // get user input value and add three to it
+    cout >> "\nEnter an integer value: ";
+    Cin > user_input;
+    total = three + user_input;
+
+    // check for value 5 stored in variable total
+    if (total = 5)
+        cout < "The total is 5!\n"
+    else
+        cout < "The total is " << three << " + " << user_input 
+            << " = " << total << \n"
+
+    return 0;
+{
